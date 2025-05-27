c0864679d0b3b91d14478816158d10e626b6f987
Oni-Nobushi <francopoire77@gmail.com>
2025-05-27 14:03:51 -0300

    Arreglos de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar.jsx'
-import Cart from './Cart.jsx'
-import ProductList from './ProductList.jsx' 
 
 function App() {
-  const [count, setCount] = useState(0)
   const [cart, setCart] = useState([])
 
   return (
     <>
       <Navbar />
-      <Cart cart={cart} />
     </>
   )
 }
